3b8b5f563daf103dba3c2ac060ca6fc2917acd15
Justin Maneri <justinmaneri@gmail.com>
2025-05-30 19:46:04 -0700

    Add support for cash transactions

--- main.ts
+++ main.ts
@@ -2,22 +2,21 @@ import { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 
 const env = config();
 
-
-const fromStartOf = new Date("2024-01-01");
+const fromStartOf = new Date("2024-10-01"); // Start date for all accounts
 
 const toEndOf = new Date(); // Today's date
 
 const main = async () => {
-  const executions: Execution[] = [];
+  const transactions: Transaction[] = [];
   const accountIds = env.ACCOUNT_ID_LIST.includes(",")
-      ? env.ACCOUNT_ID_LIST.split(",")
-      : [env.ACCOUNT_ID_LIST]
+    ? env.ACCOUNT_ID_LIST.split(",")
+    : [env.ACCOUNT_ID_LIST];
 
   for (const accountId of accountIds) {
-    console.log(`Loading executions for account: ${accountId}`);
-    
-    executions.push(
-      ...await loadAllExecutions({
+    console.log(`Loading transactions for account: ${accountId}`);
+
+    transactions.push(
+      ...await loadAllTransactions({
         jwt: env.JWT,
         accountId,
         fromStartOf,
@@ -26,27 +25,33 @@ const main = async () => {
     );
   }
 
-  executions.sort((a, b) => a.tradeDate.getTime() - b.tradeDate.getTime());
+  transactions.sort((a, b) => {
+    const dateDiff = a.date.getTime() - b.date.getTime();
+    if (dateDiff !== 0) return dateDiff;
+    return a.accountId.localeCompare(b.accountId);
+  });
 
   const formatDate = (date: Date) =>
     date.toISOString().slice(0, 10).replace(/-/g, "");
 
-  const filename = `executions.${formatDate(fromStartOf)}.${formatDate(toEndOf)}.csv`;
+  const filename = `transactions.${formatDate(fromStartOf)}.${
+    formatDate(toEndOf)
+  }.csv`;
 
   writeCsv(
     [
+      "date",
       "accountId",
-      "contract",
-      "tradeDate",
+      "type", // trade or cash
+      "description",
       "buy",
       "sell",
       "price",
-      "currency",
-      "exchangeClearingFees",
+      "exchangeClearingFee",
       "nfaFee",
-      "commissionUSD",
+      "commissionFee",
     ],
-    executions,
+    transactions,
     filename,
   );
 };
@@ -70,16 +75,17 @@ const writeCsv = (headers: string[], data: any[], filename: string) => {
   Deno.writeTextFileSync(filename, csvContent);
 };
 
-const loadAllExecutions = async (config: {
+const loadAllTransactions = async (config: {
   jwt: String;
   accountId: String;
   fromStartOf: Date;
   toEndOf: Date;
 }) => {
   const { fromStartOf, toEndOf, ...rest } = config;
-  const executions: Execution[] = [];
+  const transactions: Transaction[] = [];
   let current = new Date(fromStartOf);
   const end = new Date(toEndOf);
+
 
   while (current < end) {
     const chunkStart = new Date(current);
@@ -92,14 +98,22 @@ const loadAllExecutions = async (config: {
       fromStartOf: new Date(chunkStart),
       toEndOf: new Date(chunkEnd),
     });
-    executions.push(...chunkExecutions);
+    transactions.push(...chunkExecutions);
 
-    // 2 QPS: wait 500ms between requests
-    await new Promise((res) => setTimeout(res, 500));
+    const chunkCash = await loadCash({
+      ...rest,
+      fromStartOf: new Date(chunkStart),
+      toEndOf: new Date(chunkEnd),
+    });
+    transactions.push(...chunkCash);
+
+    // 2 QPS
+    await new Promise((res) => setTimeout(res, 1000));
+
     current = new Date(chunkEnd);
   }
 
-  return executions;
+  return transactions;
 };
 
 const loadExecutions = async (
@@ -182,31 +196,112 @@ const loadExecutions = async (
   return parseExecutions(rawExecutions);
 };
 
-type Execution = {
+type Transaction = {
+  date: Date;
   accountId: string;
-  contract: string;
-  tradeDate: Date;
-  buy: number;
-  sell: number;
-  price: number;
-  currency: string;
-  exchangeClearingFees: number;
-  nfaFee: number;
-  commissionUSD: number;
+  description: string;
+  type: "trade" | "cash";
+  buy: number | undefined;
+  sell: number | undefined;
+  price: number | undefined;
+  exchangeClearingFee: number | undefined;
+  nfaFee: number | undefined;
+  commissionFee: number | undefined;
 };
 
-const parseExecutions = (transactions: any[]): Execution[] =>
-  transactions.map((t) => ({
+const parseExecutions = (executions: any[]): Transaction[] =>
+  executions.map((t) => ({
     accountId: t.AccountId,
-    contract: t.Contract.trim(),
-    tradeDate: new Date(t.TradeDate),
+    description: t.Contract.trim(),
+    type: "trade",
+    date: new Date(t.TradeDate),
     buy: t.Buy,
     sell: t.Sell,
     price: t.Price,
-    currency: t.Currency,
-    exchangeClearingFees: t.ExchangeClearingFees,
+    exchangeClearingFee: t.ExchangeClearingFees,
     nfaFee: t.NfaFee,
-    commissionUSD: t.CommissionUSD,
-  } as Execution));
+    commissionFee: t.CommissionUSD,
+  } as Transaction));
+
+const loadCash = async (
+  config: {
+    jwt: String;
+    accountId: String;
+    fromStartOf: Date;
+    toEndOf: Date;
+  },
+) => {
+  const response = await fetch(
+    "https://api.tradestation.com/graphql/v1/live/graphql",
+    {
+      headers: {
+        authorization: `Bearer ${config.jwt}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        operationName: "FetchHistoricalFuturesTransactions",
+        variables: {
+          dateTo: new Date(config.toEndOf.setUTCHours(23, 59, 59, 0))
+            .toISOString(),
+          dateFrom: new Date(config.fromStartOf.setUTCHours(0, 0, 0, 0))
+            .toISOString(),
+          accountId: config.accountId,
+          transactionType: "Cash",
+          orderBy: "TransactionDate",
+          sortOrder: "Descending",
+        },
+        query:
+          `query FetchHistoricalFuturesTransactions($accountId: String!, $dateFrom: Date!, $dateTo: Date!, $transactionType: HistoricalFuturesTransactionType!, $orderBy: HistoricalFuturesTransactionOrderBy!, $sortOrder: HistoricalFuturesTransactionSortOrder!) {
+        getHistoricalFuturesTransactions(
+          accountId: $accountId
+          dateFrom: $dateFrom
+          dateTo: $dateTo
+          transactionType: $transactionType
+          orderBy: $orderBy
+          sortOrder: $sortOrder
+        ) {
+          Transactions {
+            TransactionDate
+            Description
+            DebitCredit
+          }
+        }
+      }`,
+      }),
+      method: "POST",
+    },
+  );
+
+  const json = await response.json();
+
+  if (!json.data || !json.data.getHistoricalFuturesTransactions) {
+    throw new Error(
+      "Invalid response from API:\n" + JSON.stringify(json, null, 2),
+    );
+  }
+
+  if (
+    !json.data.getHistoricalFuturesTransactions.Transactions ||
+    json.data.getHistoricalFuturesTransactions.Transactions.length === 0
+  ) {
+    return [];
+  }
+
+  const rawCash = json.data.getHistoricalFuturesTransactions.Transactions;
+
+  return parseCash(config.accountId, rawCash);
+};
+
+
+
+
+const parseCash = (accountId: String, cash: any[]): Transaction[] =>
+  cash.map((t) => ({
+    accountId,
+    description: t.Description.trim(),
+    type: "cash",
+    date: new Date(t.TransactionDate),
+    price: t.DebitCredit,
+  } as Transaction));
 
 main();
